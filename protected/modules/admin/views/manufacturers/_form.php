5213417953061b6d7b8462b3d206cee3c5fac8ea
damnpoet <damnpoet@gmail.com>
2013-07-19 10:24:16 -0500

    Backend updated

--- protected/modules/admin/views/manufacturers/_form.php
+++ protected/modules/admin/views/manufacturers/_form.php
@@ -42,7 +42,7 @@ $form = $this->beginWidget('CActiveForm', array(
                       <img alt="" src="<?php if(!is_null($model->getManufacturer())) echo $model->getManufacturer()->getImageWithSize(100, 100); ?>">
                       <div class="caption">
                         <p>
-                            <a class="btn btn-mini btn-primary" href="#">Browse</a> 
+                            <a class="btn btn-mini btn-primary" href="#" onclick="image_upload('image', 'thumb');">Browse</a> 
                             <a class="btn btn-mini" href="#">Clear</a>
                         </p>
                       </div>
@@ -61,3 +61,33 @@ $form = $this->beginWidget('CActiveForm', array(
 </div>
 
 <?php $this->endWidget(); ?>
+
+<script type="text/javascript"><!--
+function image_upload(field, thumb) {
+	$('#dialog').remove();
+	
+	$('#content').prepend('<div id="dialog" style="padding: 3px 0px 0px 0px;"><iframe src="index.php?route=common/filemanager&token=cbfdeb8903673d59dc56232e9ba564a4&field=' + encodeURIComponent(field) + '" style="padding:0; margin: 0; display: block; width: 100%; height: 100%;" frameborder="no" scrolling="auto"></iframe></div>');
+	
+	$('#dialog').dialog({
+		title: 'Image Manager',
+		close: function (event, ui) {
+			if ($('#' + field).attr('value')) {
+				$.ajax({
+					url: 'index.php?route=common/filemanager/image&token=cbfdeb8903673d59dc56232e9ba564a4&image=' + encodeURIComponent($('#' + field).attr('value')),
+					dataType: 'text',
+					success: function(text) {
+						$('#' + thumb).replaceWith('<img src="' + text + '" alt="" id="' + thumb + '" />');
+					}
+				});
+			}
+		},	
+		bgiframe: false,
+		width: 800,
+		height: 400,
+		resizable: false,
+		modal: false
+	});
+        
+        return false;
+};
+//--></script>
