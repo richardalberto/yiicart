16c76d1e620f7f9719a86670bb506154116e0b71
damnpoet <damnpoet@gmail.com>
2013-08-02 10:03:23 -0500

    Backend: added pagination to reviews

--- protected/modules/admin/controllers/ReviewsController.php
+++ protected/modules/admin/controllers/ReviewsController.php
@@ -3,10 +3,18 @@
 class ReviewsController extends BackendController {
 
     public function actionIndex() {
-        $reviews = Review::model()->findAll();
+        $criteria = new CDbCriteria();
+        $count = Review::model()->count($criteria);
+        $pages = new CPagination($count);
+
+        // results per page
+        $pages->pageSize = Yii::app()->settings->getValue('config_admin_limit');
+        $pages->applyLimit($criteria);
+        $reviews = Review::model()->findAll($criteria);
         
         $this->render('index', array(
-            'reviews'=>$reviews            
+            'reviews'=>$reviews,
+            'pages'=>$pages
         ));
     }
 
