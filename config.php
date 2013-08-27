1910e2c96a54a5fcbdd524455326bb159f097f7f
damnpoet <damnpoet@gmail.com>
2013-08-27 14:27:40 -0500

    updated distribution config

--- config.php
+++ config.php
@@ -4,21 +4,21 @@ return array(
     'name' => 'Yii Cart',
     'language'=>'en',
     'theme'=>'classic',
-    
+
     // application components
     'components' => array(
         'db' => array(
-            'connectionString' => 'mysql:host=localhost;dbname=yiicart',
+            'connectionString' => 'mysql:host=localhost;dbname=dname',
             'emulatePrepare' => true,
             'username' => 'username',
             'password' => 'password',
             'charset' => 'utf8',
         ),
     ),
-    
-    // application-level parameters that can be accessed
-    // using Yii::app()->params['paramName']
+
+    // global params
     'params' => array(
-        'imagePath' => '/var/www/htdocs/YiiCart/image/',
+        'imagePath' => '/var/www/yiicart/image',
     ),
 );
+?>
