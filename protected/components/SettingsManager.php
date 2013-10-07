8f23eb22319f8de97e3991e006e879fae9c40026
damnpoet <damnpoet@gmail.com>
2013-10-07 16:42:43 -0400

    Added setValue function
    setValue will aid in the update and creation of system settings.

--- protected/components/SettingsManager.php
+++ protected/components/SettingsManager.php
@@ -10,5 +10,18 @@ class SettingsManager extends CComponent{
         $setting = Setting::model()->findByAttributes(array('key'=>$settingName, 'store_id'=>$storeId, 'group'=>$group));
         return !is_null($setting) ? $setting->value : null;
     }
+    
+    public function setValue($settingName, $settingValue, $storeId = 0, $group='config'){
+        $setting = Setting::model()->findByAttributes(array('key'=>$settingName, 'store_id'=>$storeId, 'group'=>$group));
+        if(is_null($setting)) {
+            $setting = new Setting;
+            $setting->key = $settingName;
+            $setting->store_id = $storeId;
+            $setting->group = $group;
+        }
+        
+        $setting->value = $settingValue;
+        $setting->save();
+    }
 }
 ?>
