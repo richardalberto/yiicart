ceacdd983bf3bfe9974bee9844b5e0685011325d
Richard González Alberto <damnpoet@gmail.com>
2014-02-07 09:09:12 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// This is the main YiiCart configuration.
+return array(
+    'name' => 'Yii Cart',
+    'language'=>'en',
+    'theme'=>'classic',
+
+    // application components
+    'components' => array(
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=yiicart',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ),
+    ),
+    
+    // global params
+    'params' => array(
+        'imagePath' => '/var/www/yiicart/image/',
+    ),
+);
+?>
